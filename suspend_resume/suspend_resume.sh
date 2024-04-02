d583be26cbad1705105163cf46dd420711276042
Caesar Wang <wxt@rock-chips.com>
2024-04-02 16:17:40 +0800

    suspend_resume: update suspend_resume.sh
    To fix the issue where the RTC cannot auto-wake, it’s necessary to ensure
    that the time set for the RTC is at least 1 minute.
    
    Change-Id: I124524888010058810fc32c3e16c18cd429defea
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- suspend_resume/suspend_resume.sh
+++ suspend_resume/suspend_resume.sh
@@ -2,52 +2,55 @@
 
 RESULT_DIR=/userdata/rockchip-test/
 RESULT_LOG=${RESULT_DIR}/suspend_resume.txt
-SUSPEND_MAX=60
-SUSPEND_MIN=30
-SUSPEND_INTERVAL=$(($SUSPEND_MAX - $SUSPEND_MIN + 1 ))
-WAKE_MAX=60
-WAKE_MIN=30
-WKAE_INTERVAL=$(($WAKE_MAX - $WAKE_MIN + 1 ))
+SUSPEND_MIN=60  # Ensure minimum suspend time is 60 seconds
+WAKE_MIN=5
 MAX_CYCLES=10000
 
-if [ ! -e "/sys/class/rtc/rtc0/wakealarm" ];then
-    echo "non-existent rtc, please check if rtc enabled"
-    exit
+# Enable Debug
+# echo N > /sys/module/printk/parameters/console_suspend
+# echo 1 > /sys/power/pm_print_times
+
+# Check if RTC is present
+if [ ! -e "/sys/class/rtc/rtc0/wakealarm" ]; then
+    echo "RTC not present, please check if RTC is enabled"
+    exit 1
 fi
 
+# Create result directory
 mkdir -p ${RESULT_DIR}
 
+# Function to generate random number
 random() {
   hexdump -n 2 -e '/2 "%u"' /dev/urandom
 }
 
-auto_suspend_resume_rtc()
-{
+auto_suspend_resume_rtc() {
     cnt=0
 
-    # set sys time same with rtc
+    # Sync system time to RTC
     hwclock --systohc
-    hwclock -w
-    echo "$(date): auto_suspend_resume_rtc start" > ${RESULT_LOG}
+    echo "$(date): auto_suspend_resume_rtc starts" > ${RESULT_LOG}
 
-    while true; do
-        echo "have done $cnt suspend/resume"
-        if [ $cnt -ge $MAX_CYCLES ]
-        then
-            echo "run $MAX_CYCLES cycles, finish test"
-            exit 0
-        fi
-        sus_time=$(( ( $(random) % $SUSPEND_INTERVAL ) + $SUSPEND_MIN ))
-        echo "sleep for $sus_time second"
+    while [ $cnt -lt $MAX_CYCLES ]; do
+        echo "Completed $cnt suspend/resume cycles"
+
+        # Calculate suspend time to ensure it's at least 60 seconds
+        sus_time=$(( ( $(random) % $SUSPEND_MIN ) + $SUSPEND_MIN ))
+        echo "Sleeping for $sus_time seconds"
+        # Clear previous wakealarm and set new wake time
         echo 0 > /sys/class/rtc/rtc0/wakealarm
         echo "+${sus_time}" > /sys/class/rtc/rtc0/wakealarm
         pm-suspend
-        wake_time=$(( ( $(random) % $WKAE_INTERVAL ) + $WAKE_MIN ))
-        echo "wake for $wake_time second"
+
+        # Calculate wake time, ensuring at least 30 seconds of wake time
+        wake_time=$(( ( $(random) % $WAKE_MIN ) + $WAKE_MIN ))
+        echo "Waking for $wake_time seconds"
         sleep $wake_time
-        echo "$(date): Count: $cnt - sleep: $sus_time wake: $wake_time " >> ${RESULT_LOG}
-        let "cnt=$cnt+1"
+
+        echo "$(date): Cycle: $cnt - Sleep: $sus_time Wake: $wake_time" >> ${RESULT_LOG}
+
+        cnt=$((cnt + 1))
     done
 }
-auto_suspend_resume_rtc
 
+auto_suspend_resume_rtc
